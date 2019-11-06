1068a6a6b9007efe1fe8320439fb738cae74eeef
ivandermoura <ivanderlp1@gmail.com>
2019-11-06 15:16:42 +0700

    update testing kedua

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,38 +17,8 @@ void main(){
   });
    
  
-  testWidgets('Add and remove a todo', (WidgetTester tester) async {
-   
-    // Build the widget.
-    await tester.pumpWidget(Luassegiempat());
-    // Enter 'hi' into the TextField.
-    
-    await tester.press(find.byKey(Key('luas')));
   
-    expect(find.byKey(Key('luas')), findsOneWidget);
-    expect(find.byKey(Key('keliling')), findsNothing);
 
-    await tester.pump();
-    
-    
-});
-
-testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(Luassegiempat());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
 
 
 
