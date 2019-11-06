879c5b3a08170f25b078d8da2b8cdf2bd3ffa9c2
ivandermoura <ivanderlp1@gmail.com>
2019-11-06 15:27:22 +0700

    update testing_dart

--- test/testing_test.dart
+++ test/testing_test.dart
@@ -4,18 +4,6 @@ import 'package:lat_pp/main.dart';
 void main(){
   testWidgets('tambah atau hapus todo', (WidgetTester tester) async {
    
-    // Build the widget.
-    await tester.pumpWidget(Luassegiempat());
-    // Enter 'hi' into the TextField.
-    
-    await tester.press(find.byKey(Key('luas')));
-  
-    expect(find.byKey(Key('luas')), findsOneWidget);
-    expect(find.byKey(Key('keliling')), findsNothing);
-
-    await tester.pump();
-    
-    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Luassegiempat());
 
@@ -33,5 +21,4 @@ void main(){
   });
 
     
-});
 }
